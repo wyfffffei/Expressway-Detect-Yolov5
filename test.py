565b9ffc8aff517ed6ac1d602cd53b27292b04d4
wyfffffei <h3wyf@126.com>
2022-03-22 01:59:51 +0800

    add data_migration & extract_pictures_from_video

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import torch
 
-print(torch.cuda.is_available())
+# print(torch.cuda.is_available())
+
+
+save_path = "D:/projects/datasets/EXP_Detect/static/"
+file = open(save_path + "test.txt", 'a+')
+file.writelines("123" + "\n")
+file.close()
+
+file_2 = open(save_path + "test.txt", 'a+')
+file_2.writelines("456" + "\n")
+file_2.close()
--- utils/datatransform.py
+++ utils/datatransform.py
@@ -5,6 +5,7 @@
 @ Time: 2022/3/17 18:39
 @ Function: change the annotation format
 """
+import os
 import json
 import numpy as np
 from tqdm import tqdm
@@ -38,24 +39,34 @@ def panda2YOLO():
     To YOLO Format:
         class_index x_center y_center width height
     """
-    ano_file_path = "D:/projects/datasets/panda/PANDA_Crowd/head_bbox_annotations.json"
+    # ano_file_path = "D:/projects/datasets/panda/PANDA_Crowd/head_bbox_annotations.json"
+    # ano_file_path = "D:/projects/datasets/panda/Panda-Image/PANDA_IMAGE/image_annos/person_bbox_train.json"
+    ano_file_path = "D:/projects/datasets/panda/Panda-Image/PANDA_IMAGE/image_annos/vehicle_bbox_train.json"
     save_path = "D:/projects/datasets/EXP_Detect/static/annotations/"
-    names = ['person', 'car', 'fire', 'rubbish']  # class names
 
+    # names = ['person', 'car', 'fire', 'rubbish']  # class names
+    cla_index = ('0', '1', '2', '3')
+    classes = {"people": ("person", "people", "person head"),
+               "car": ("large car", "midsize car", "small car", "electric car", "vehicles", "motorcycle")}
+
+    pics = set()
     with open(ano_file_path) as data:
         ano = json.load(data)
         for name, item in ano.items():
-            save_ano = open(save_path + name.split("/")[1][:-4] + '.txt', 'w')
+            pics.add(name)
             obj_list = item["objects list"]
+            saved_ano = open(save_path + name.split("/")[1][:-4] + '.txt', 'a+')
 
             num_ano = 0
             # 逐行写入 YOLO 格式标记框信息
             for obj in tqdm(obj_list):
                 # 格式参考 http://www.panda-dataset.com/Download.html
-                if obj["category"] == "person":
-                    rect = obj["rects"]["full body"]
-                elif obj["category"] == "people" or obj["category"] == "person head":
+                if obj["category"] in classes["people"]:
+                    rect = obj["rects"]["full body"] if obj["category"] == "person" else obj["rect"]
+                    cla = cla_index[0]
+                elif obj["category"] in classes["car"]:
                     rect = obj["rect"]
+                    cla = cla_index[1]
                 else:
                     continue
                 x_center = str((rect['tl']['x'] + rect['br']['x']) / 2)
@@ -63,12 +74,12 @@ def panda2YOLO():
                 width = str(np.abs(rect['tl']['x'] - rect['br']['x']))
                 height = str(np.abs(rect['tl']['y'] - rect['br']['y']))
 
-                # TODO: 类别判断
-                save_ano.writelines('0 ' + x_center + ' ' + y_center + ' ' + width + ' ' + height + "\n")
+                saved_ano.writelines(cla + ' ' + x_center + ' ' + y_center + ' ' + width + ' ' + height + "\n")
                 num_ano += 1
 
             print("{} -> length: {}".format(str(name), str(num_ano)))
-            save_ano.close()
+            saved_ano.close()
+    return pics
 
 
 def show_category():
@@ -89,11 +100,80 @@ def show_category():
     print(category)
 
 
-def test():
+def data_migration(pics):
+    """
+    :function: copy train pictures (windows script)
+    :param pics: picture name set
+    """
+    parent = "D:\\projects\\datasets\\panda\\Panda-Image\\PANDA_IMAGE\\image_train\\"
+    copy_path = "D:\\projects\\datasets\\EXP_Detect\\static\\train\\"
+    for pic in pics:
+        pic = pic.replace('/', '\\')
+        os.system(f"copy {parent+pic} {copy_path}")
+
+
+def extract_pictures_from_video():
+    """
+    :function: extract 10 pictures from each video (00:00, 00:30, 01:00, ...) (windows script)
+    TODO: divide the video by the percentage (some video < 1 min)
+    :required: ffmpeg -> https://gist.github.com/steven2358/ba153c642fe2bb1e47485962df07c730
+    """
+    v_parent = "D:\\Document\\res_DeepLearning\\Video1\\"
+    # v_parent = "D:\\Document\\res_DeepLearning\\Video2\\"
+    val_save_path = "D:\\projects\\datasets\\EXP_Detect\\static\\val\\"
+    test_save_path = "D:\\projects\\datasets\\EXP_Detect\\static\\test\\"
+
+    for file in os.listdir(v_parent):
+        if file[-4:] != '.mp4':
+            file = file + '\\' + os.listdir(v_parent+file)[0]
+
+        for time in range(10):
+            if time % 2 == 0:
+                os.system("ffmpeg -i {} -ss 00:0{}:00.000 -vframes 1 {}{}_00{}.jpg -hide_banner".format(
+                    v_parent+file, int(time/2), val_save_path, file.split('\\')[-1][:-4], time
+                ))
+            else:
+                os.system("ffmpeg -i {} -ss 00:0{}:30.000 -vframes 1 {}{}_00{}.jpg -hide_banner".format(
+                    v_parent+file, time//2, test_save_path, file.split('\\')[-1][:-4], time
+                ))
+
+
+def data_split(data):
+    # train : val : test  <==>  all_panda + 20% highway_pic : 40% highway_pic : 40% highway_pic
+    # TODO: split the train data to train / val (files / annotations)
     pass
 
 
+def test():
+    v_parent = "D:\\Document\\res_DeepLearning\\Video1\\"
+    val_save_path = "D:\\projects\\datasets\\EXP_Detect\\static\\val\\"
+    test_save_path = "D:\\projects\\datasets\\EXP_Detect\\static\\test\\"
+    for file in os.listdir(v_parent):
+        if file[-4:] != '.mp4':
+            file = file + '\\' + os.listdir(v_parent+file)[0]
+
+        for time in range(10):
+            if time % 2 == 0:
+                print("ffmpeg -i {} -ss 00:0{}:00.000 -vframes 1 {}{}_00{}.jpg -hide_banner".format(
+                    v_parent+file, int(time/2), val_save_path, file.split('\\')[-1][:-4], time
+                ))
+            else:
+                print("ffmpeg -i {} -ss 00:0{}:30.000 -vframes 1 {}{}_00{}.jpg -hide_banner".format(
+                    v_parent+file, time//2, test_save_path, file.split('\\')[-1][:-4], time
+                ))
+
+
+def main():
+    test()
+
+    # show_category()
+    # train_pics = panda2YOLO()
+    # print(train_pics)
+    # data_migration(train_pics)
+    # extract_pictures_from_video()
+    # data_split(train_pics)
+
+
 if __name__ == "__main__":
-    show_category()
-    # panda2YOLO()
+    main()
 
